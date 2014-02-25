924496a62a735f5d66c3850d464399da68a71122
He Liu <liulonnie@gmail.com>
2014-02-25 14:00:04 -0800

    main

--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
-	"log"
-	"fmt"
 	"flag"
+	"fmt"
 	"go/build"
+	"log"
 )
+
+type pkgList struct {
+	pmap  map[string]*build.Package
+	plist []string
+}
+
+func newPkgList() *pkgList {
+	ret := new(pkgList)
+	ret.pmap = make(map[string]*build.Package)
+	ret.plist = make([]string, 0, 100)
+	return ret
+}
+
+func (self *pkgList) have(p string) bool {
+	return self.pmap[p] != nil
+}
+
+func (self *pkgList) add(p string, pkg *build.Package) bool {
+	ret := self.have(p)
+	if !ret {
+		self.pmap[p] = pkg
+		self.plist = append(self.plist, p)
+	}
+	return ret
+}
 
 func main() {
 	flag.Parse()
@@ -20,7 +45,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	for name, _ := range pkgs {
+	for _, name := range pkgs.plist {
 		if name == target {
 			continue
 		}
@@ -28,24 +53,23 @@ func main() {
 	}
 }
 
-func findDeps(p string) (map[string]*build.Package, error) {
-	ret := make(map[string]*build.Package)
+func findDeps(p string) (*pkgList, error) {
+	ret := newPkgList()
 	err := addDeps(ret, p)
 	return ret, err
 }
 
-func addDeps(pkgs map[string]*build.Package, p string) error {
+func addDeps(pkgs *pkgList, p string) error {
 	pkg, err := build.Import(p, "./", 0)
 	if err != nil {
 		return fmt.Errorf("failed to import %s: %v", p, err)
 	}
 
-	if _, found := pkgs[pkg.ImportPath]; found {
+	if pkgs.have(p) {
 		return nil
 	}
 
-	pkgs[pkg.ImportPath] = pkg
-	
+	pkgs.add(p, pkg)
 	for _, imp := range pkg.Imports {
 		err = addDeps(pkgs, imp)
 		if err != nil {
